beeeea0d71fd1594cf86f3a8d18c9652845fe118
unc0 <undercooled@lavabit.com>
2013-08-06 18:39:15 +0800

    Clean whitespaces, add some features.
    * Using Dom element as the first argument of crel()
    * Using Dom elements as the third argument of crel()

--- crel.js
+++ crel.js
@@ -9,28 +9,28 @@
 /*
 
     This code is not formatted for readability, but rather run-speed and to assist compilers.
-    
+
     However, the code's intention should be transparent.
-    
+
     *** IE SUPPORT ***
-    
+
     If you require this library to work in IE7, add the following after declaring crel.
-    
+
     var testDiv = document.createElement('div'),
         testLabel = document.createElement('label');
 
-    testDiv.setAttribute('class', 'a');    
+    testDiv.setAttribute('class', 'a');
     testDiv['className'] !== 'a' ? crel.attrMap['class'] = 'className':undefined;
     testDiv.setAttribute('name','a');
     testDiv['name'] !== 'a' ? crel.attrMap['name'] = function(element, value){
         element.id = value;
     }:undefined;
-    
+
 
     testLabel.setAttribute('for', 'a');
     testLabel['htmlFor'] !== 'a' ? crel.attrMap['for'] = 'htmlFor':undefined;
-    
-    
+
+
 
 */
 
@@ -45,75 +45,86 @@
 }(this, function () {
     // based on http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
     var isNode = typeof Node === 'object'
-        ? function (object) { return object instanceof Node }
+        ? function (object) { return object instanceof Node; }
         : function (object) {
             return object
                 && typeof object === 'object'
                 && typeof object.nodeType === 'number'
                 && typeof object.nodeName === 'string';
         };
+    var isArray = function(a){ return a instanceof Array; };
+    var appendChild = function(element, child) {
+      if(!isNode(child)){
+          child = document.createTextNode(child);
+      }
+      element.appendChild(child);
+    };
+
 
     function crel(){
         var document = window.document,
             args = arguments, //Note: assigned to a variable to assist compilers. Saves about 40 bytes in closure compiler. Has negligable effect on performance.
-            element = document.createElement(args[0]),
+            element = args[0],
             child,
             settings = args[1],
             childIndex = 2,
             argumentsLength = args.length,
             attributeMap = crel.attrMap;
 
+        element = isNode(element) ? element : document.createElement(element);
         // shortcut
         if(argumentsLength === 1){
             return element;
         }
 
-        if(typeof settings !== 'object' || isNode(settings)) {
+        if(typeof settings !== 'object' || isNode(settings) || isArray(settings)) {
             --childIndex;
             settings = null;
         }
 
-        // shortcut if there is only one child that is a string    
+        // shortcut if there is only one child that is a string
         if((argumentsLength - childIndex) === 1 && typeof args[childIndex] === 'string' && element.textContent !== undefined){
             element.textContent = args[childIndex];
-        }else{    
+        }else{
             for(; childIndex < argumentsLength; ++childIndex){
                 child = args[childIndex];
-                
+
                 if(child == null){
                     continue;
                 }
-                
-                if(!isNode(child)){
-                    child = document.createTextNode(child);
+
+                if (isArray(child)) {
+                  for (var i=0; i < child.length; ++i) {
+                    appendChild(element, child[i]);
+                  }
+                } else {
+                  appendChild(element, child);
                 }
-                
-                element.appendChild(child);
             }
         }
-        
+
         for(var key in settings){
             if(!attributeMap[key]){
                 element.setAttribute(key, settings[key]);
             }else{
                 var attr = crel.attrMap[key];
-                if(typeof attr === 'function'){     
-                    attr(element, settings[key]);               
-                }else{            
+                if(typeof attr === 'function'){
+                    attr(element, settings[key]);
+                }else{
                     element.setAttribute(attr, settings[key]);
                 }
             }
         }
-        
+
         return element;
     }
-    
+
     // Used for mapping one kind of attribute to the supported version of that in bad browsers.
     // String referenced so that compilers maintain the property name.
     crel['attrMap'] = {};
-    
+
     // String referenced so that compilers maintain the property name.
     crel["isNode"] = isNode;
-    
+
     return crel;
 }));
