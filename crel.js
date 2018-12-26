16439a4f3d123fd486e72c5a8faf68c969d3542a
Dev_Bon_Bon <frapul2gar@live.fi>
2018-12-26 03:28:42 +0200

    Crel: conform to js-semistandard style

--- crel.js
+++ crel.js
@@ -1,166 +1,138 @@
-//Copyright (C) 2012 Kory Nunn
+/* Copyright (C) 2012 Kory Nunn
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-/*
-
-    This code is not formatted for readability, but rather run-speed and to assist compilers.
-
-    However, the code's intention should be transparent.
-
-    *** IE SUPPORT ***
-
-    If you require this library to work in IE7, add the following after declaring crel.
-
-    var testDiv = document.createElement('div'),
-        testLabel = document.createElement('label');
-
-    testDiv.setAttribute('class', 'a');
-    testDiv['className'] !== 'a' ? crel.attrMap['class'] = 'className':undefined;
-    testDiv.setAttribute('name','a');
-    testDiv['name'] !== 'a' ? crel.attrMap['name'] = function(element, value){
-        element.id = value;
-    }:undefined;
-
-
-    testLabel.setAttribute('for', 'a');
-    testLabel['htmlFor'] !== 'a' ? crel.attrMap['for'] = 'htmlFor':undefined;
-
-
-
-*/
+NOTE:
+This code is not formatted for readability, but rather run-speed and to assist compilers.
+However, the code's intention should be transparent. */
 
 (function (root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        root.crel = factory();
-    }
+  if (typeof exports === 'object') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else {
+    root.crel = factory();
+  }
 }(this, function () {
-    var fn = 'function',
-        obj = 'object',
-        nodeType = 'nodeType',
-        textContent = 'textContent',
-        setAttribute = 'setAttribute',
-        attrMapString = 'attrMap',
-        isNodeString = 'isNode',
-        isElementString = 'isElement',
-        d = typeof document === obj ? document : {},
-        isType = function(a, type){
-            return typeof a === type;
-        },
-        isNode = typeof Node === fn ? function (object) {
-            return object instanceof Node;
-        } :
-        // in IE <= 8 Node is an object, obviously..
-        function(object){
-            return object &&
-                isType(object, obj) &&
-                (nodeType in object) &&
-                isType(object.ownerDocument,obj);
-        },
-        isElement = function (object) {
-            return crel[isNodeString](object) && object[nodeType] === 1;
-        },
-        isArray = function(a){
-            return a instanceof Array;
-        },
-        appendChild = function(element, child) {
-            if (isArray(child)) {
-                child.map(function(subChild){
-                    appendChild(element, subChild);
-                });
-                return;
-            }
-            if(!crel[isNodeString](child)){
-                child = d.createTextNode(child);
-            }
-            element.appendChild(child);
-        };
+  var fn = 'function';
+  var obj = 'object';
+  var nodeType = 'nodeType';
+  var textContent = 'textContent';
+  var setAttribute = 'setAttribute';
+  var attrMapString = 'attrMap';
+  var isNodeString = 'isNode';
+  var isElementString = 'isElement';
+  var d = typeof document === obj ? document : {};
+  var isType = function (a, type) {
+    return typeof a === type;
+  };
+  var isNode = typeof Node === fn ? function (object) {
+    return object instanceof Node;
+  }
+    // in IE <= 8 Node is an object, obviously..
+    : function (object) {
+      return object &&
+        isType(object, obj) &&
+        (nodeType in object) &&
+        isType(object.ownerDocument, obj);
+    };
+  var isElement = function (object) {
+    return crel[isNodeString](object) && object[nodeType] === 1;
+  };
+  var isArray = function (a) {
+    return a instanceof Array;
+  };
+  var appendChild = function (element, child) {
+    if (isArray(child)) {
+      child.map(function (subChild) {
+        appendChild(element, subChild);
+      });
+      return;
+    }
+    if (!crel[isNodeString](child)) {
+      child = d.createTextNode(child);
+    }
+    element.appendChild(child);
+  };
 
+  function crel () {
+    var args = arguments; // Note: assigned to a variable to assist compilers. Saves about 40 bytes in closure compiler. Has negligable effect on performance.
+    var element = args[0];
+    var child;
+    var settings = args[1];
+    var childIndex = 2;
+    var argumentsLength = args.length;
+    var attributeMap = crel[attrMapString];
 
-    function crel(){
-        var args = arguments, //Note: assigned to a variable to assist compilers. Saves about 40 bytes in closure compiler. Has negligable effect on performance.
-            element = args[0],
-            child,
-            settings = args[1],
-            childIndex = 2,
-            argumentsLength = args.length,
-            attributeMap = crel[attrMapString];
-
-        element = crel[isElementString](element) ? element : d.createElement(element);
-        // shortcut
-        if(argumentsLength === 1){
-            return element;
-        }
-
-        if(!isType(settings,obj) || crel[isNodeString](settings) || isArray(settings)) {
-            --childIndex;
-            settings = null;
-        }
-
-        // shortcut if there is only one child that is a string
-        if((argumentsLength - childIndex) === 1 && isType(args[childIndex], 'string') && element[textContent] !== undefined){
-            element[textContent] = args[childIndex];
-        }else{
-            for(; childIndex < argumentsLength; ++childIndex){
-                child = args[childIndex];
-
-                if(child == null){
-                    continue;
-                }
-
-                if (isArray(child)) {
-                  for (var i=0; i < child.length; ++i) {
-                    appendChild(element, child[i]);
-                  }
-                } else {
-                  appendChild(element, child);
-                }
-            }
-        }
-
-        for(var key in settings){
-            if(!attributeMap[key]){
-                if(isType(settings[key],fn)){
-                    element[key] = settings[key];
-                }else{
-                    element[setAttribute](key, settings[key]);
-                }
-            }else{
-                var attr = attributeMap[key];
-                if(typeof attr === fn){
-                    attr(element, settings[key]);
-                }else{
-                    element[setAttribute](attr, settings[key]);
-                }
-            }
-        }
-
-        return element;
+    element = crel[isElementString](element) ? element : d.createElement(element);
+    // shortcut
+    if (argumentsLength === 1) {
+      return element;
     }
 
-    // Used for mapping one kind of attribute to the supported version of that in bad browsers.
-    crel[attrMapString] = {};
-
-    crel[isElementString] = isElement;
-
-    crel[isNodeString] = isNode;
-
-    if(typeof Proxy !== 'undefined'){
-        crel.proxy = new Proxy(crel, {
-            get: function(target, key){
-                !(key in crel) && (crel[key] = crel.bind(null, key));
-                return crel[key];
-            }
-        });
+    if (!isType(settings, obj) || crel[isNodeString](settings) || isArray(settings)) {
+      --childIndex;
+      settings = null;
     }
 
-    return crel;
+    // shortcut if there is only one child that is a string
+    if ((argumentsLength - childIndex) === 1 && isType(args[childIndex], 'string') && element[textContent] !== undefined) {
+      element[textContent] = args[childIndex];
+    } else {
+      for (; childIndex < argumentsLength; ++childIndex) {
+        child = args[childIndex];
+
+        if (child == null) {
+          continue;
+        }
+
+        if (isArray(child)) {
+          for (var i = 0; i < child.length; ++i) {
+            appendChild(element, child[i]);
+          }
+        } else {
+          appendChild(element, child);
+        }
+      }
+    }
+
+    for (var key in settings) {
+      if (!attributeMap[key]) {
+        if (isType(settings[key], fn)) {
+          element[key] = settings[key];
+        } else {
+          element[setAttribute](key, settings[key]);
+        }
+      } else {
+        var attr = attributeMap[key];
+        if (typeof attr === fn) {
+          attr(element, settings[key]);
+        } else {
+          element[setAttribute](attr, settings[key]);
+        }
+      }
+    }
+
+    return element;
+  }
+
+  // Used for mapping one kind of attribute to the supported version of that in bad browsers.
+  crel[attrMapString] = {};
+
+  crel[isElementString] = isElement;
+
+  crel[isNodeString] = isNode;
+
+  if (typeof Proxy !== 'undefined') {
+    crel.proxy = new Proxy(crel, {
+      get: function (target, key) {
+        !(key in crel) && (crel[key] = crel.bind(null, key));
+        return crel[key];
+      }
+    });
+  }
+
+  return crel;
 }));
