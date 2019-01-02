e3602fc0909736466a6f131c7bb894d4958da611
Dev_Bon_Bon <frapul2gar@live.fi>
2019-01-03 00:53:03 +0200

    Tests: Make code style consistent and concise

--- test/index.js
+++ test/index.js
@@ -1,197 +1,145 @@
 var crel = require('../'),
     test = require('tape');
 
-test('create div', function(t) {
+test('create div', function (t) {
     t.plan(1);
 
-    var testElement = crel('div', {'class':'thing'});
+    var testElement = crel('div');
 
-    t.equal(
-        testElement.tagName,
-        'DIV'
-    );
+    t.equal(testElement.tagName, 'DIV');
 
     t.end();
 });
 
-test('create div with a class', function(t) {
+test('create div with a class', function (t) {
     t.plan(1);
 
-    var testElement = crel('div', {'class':'thing'});
+    var testElement = crel('div', {'class': 'test'});
 
-    t.equal(
-        testElement.className,
-        'thing'
-    );
+    t.equal(testElement.className, 'test');
 
     t.end();
 });
 
-test('create div with a child', function(t) {
+test('create div with a child', function (t) {
     t.plan(2);
 
-    var testElement = crel('div',
-            crel('span')
-        );
+    var testElement = crel('div', crel('span'));
 
-    t.equal(
-        testElement.childNodes.length,
-        1
-    );
-
-    t.deepEqual(
-        testElement.childNodes[0].tagName,
-        'SPAN'
-    );
+    t.equal(testElement.childNodes.length, 1);
+    t.deepEqual(testElement.childNodes[0].tagName, 'SPAN');
 
     t.end();
 });
 
-test('append array children', function(t) {
+test('append array children', function (t) {
     t.plan(1);
 
     var testElement = crel('div', [1, 2, 3]);
 
-    t.equal(
-        testElement.childNodes.length,
-        3
-    );
+    t.equal(testElement.childNodes.length, 3);
 
     t.end();
 });
 
-test('append deep array children', function(t) {
+test('append deep array children', function (t) {
     t.plan(1);
 
     var testElement = crel('div', [[1, 2, 3]]);
 
-    t.equal(
-        testElement.childNodes.length,
-        3
-    );
+    t.equal(testElement.childNodes.length, 3);
 
     t.end();
 });
 
-test('add attributes to an existing element', function(t) {
+test('add attributes to an existing element', function (t) {
     t.plan(2);
 
-    var existing = document.createElement('div'),
-        testElement = crel(existing, {'class':'thing'});
+    var testElement = document.createElement('div');
+    crel(testElement, {'class': 'test'});
 
-    t.equal(
-        testElement.className,
-        'thing'
-    );
-
-    t.equal(
-        testElement.tagName,
-        'DIV'
-    );
+    t.equal(testElement.className, 'test');
+    t.equal(testElement.tagName, 'DIV');
 
     t.end();
 });
 
-test('isElement', function(t) {
+test('isElement', function (t) {
     t.plan(2);
 
-    var element = document.createElement('div'),
-        textNode = document.createTextNode('text');
+    var testElement = document.createElement('div');
+    var testNode = document.createTextNode('test');
 
     t.ok(
-        crel.isElement(element),
+        crel.isElement(testElement),
         'a <div> is an element'
     );
 
     t.notOk(
-        crel.isElement(textNode),
+        crel.isElement(testNode),
         'a textNode is not an element'
     );
 
     t.end();
 });
 
-test('isNode', function(t) {
+test('isNode', function (t) {
     t.plan(2);
 
-    var element = document.createElement('div'),
-        textNode = document.createTextNode('text');
+    var testElement = document.createElement('div');
+    var testNode = document.createTextNode('test');
 
     t.ok(
-        crel.isNode(element),
+        crel.isNode(testElement),
         'a <div> is an element'
     );
 
     t.ok(
-        crel.isNode(textNode),
+        crel.isNode(testNode),
         'a textNode is not an element'
     );
 
     t.end();
 });
 
-test('pass a text node as the first child', function(t) {
+test('pass a text node as the first child', function (t) {
     t.plan(1);
 
-    var textNode = document.createTextNode('text');
+    var testElement = crel('div', document.createTextNode('test'));
 
-    var element = crel('div', textNode);
-
-    t.equal(
-        element.childNodes.length,
-        1,
-        'textNode was appended'
-    );
+    t.equal(testElement.childNodes.length, 1);
 
     t.end();
 });
 
-test('onevent binding', function(t) {
+test('onevent binding', function (t) {
     t.plan(1);
 
-    var clickNode = crel('button', {
-        onclick: function(){
+    var testElement = crel('button', {
+        onclick: function () {
             t.pass();
         }
     });
 
-    clickNode.click();
+    testElement.click();
 
     t.end();
 });
 
-if(typeof Proxy !== 'undefined'){
-
-    test('proxy API', function(t) {
+if (typeof Proxy !== 'undefined') {
+    test('proxy API', function (t) {
         t.plan(4);
 
         var proxyCrel = crel.proxy;
 
-        var testElement = proxyCrel.div({class: 'foo'},
-                proxyCrel.span('bar')
-            );
+        var testElement = proxyCrel.div({class: 'test'},
+                proxyCrel.span('test'));
 
-        t.equal(
-            testElement.className,
-            'foo'
-        );
-
-        t.equal(
-            testElement.childNodes.length,
-            1
-        );
-
-        t.equal(
-            testElement.childNodes[0].tagName,
-            'SPAN'
-        );
-
-        t.equal(
-            testElement.childNodes[0].textContent,
-            'bar'
-        );
+        t.equal(testElement.className, 'test');
+        t.equal(testElement.childNodes.length, 1);
+        t.equal(testElement.childNodes[0].tagName, 'SPAN');
+        t.equal(testElement.childNodes[0].textContent, 'test');
 
         t.end();
     });
-
 }
