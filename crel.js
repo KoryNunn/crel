3bb94e90dd3f29f435cc6f68034f5787326519bc
Dev_Bon_Bon <frapul2gar@live.fi>
2019-01-31 02:49:54 +0200

    Crel: Turn `crel` into a `Proxy`

--- crel.js
+++ crel.js
@@ -9,7 +9,6 @@ This might make it harder to read at times, but the code's intention should be t
 
 // IIFE our function
 ((exporter) => {
-    // Define our function and its properties
     // These strings are used multiple times, so this makes things smaller once compiled
     const func = 'function',
         isNodeString = 'isNode',
@@ -28,56 +27,53 @@ This might make it harder to read at times, but the code's intention should be t
                     element.appendChild(child);
                 }
             }
-        };
-    //
-    function crel (element, settings) {
-        // Define all used variables / shortcuts here, to make things smaller once compiled
-        let args = arguments, // Note: assigned to a variable to assist compilers.
-            index = 1,
-            key,
-            attribute;
-        // If first argument is an element, use it as is, otherwise treat it as a tagname
-        element = crel.isElement(element) ? element : d.createElement(element);
-        // Check if second argument is a settings object
-        if (isType(settings, 'object') && !crel[isNodeString](settings) && !Array.isArray(settings)) {
-            // Don't treat settings as a child
-            index++;
-            // Go through settings / attributes object, if it exists
-            for (key in settings) {
-                // Store the attribute into a variable, before we potentially modify the key
-                attribute = settings[key];
-                // Get mapped key / function, if one exists
-                key = crel.attrMap[key] || key;
-                // Note: We want to prioritise mapping over properties
-                if (isType(key, func)) {
-                    key(element, attribute);
-                } else if (isType(attribute, func)) { // ex. onClick property
-                    element[key] = attribute;
-                } else {
-                    // Set the element attribute
-                    element.setAttribute(key, attribute);
+        },
+        // Define our function as a proxy interface
+        crel = new Proxy(function (element, settings) {
+            // Define all used variables / shortcuts here, to make things smaller once compiled
+            let args = arguments, // Note: assigned to a variable to assist compilers.
+                index = 1,
+                key,
+                attribute;
+            // If first argument is an element, use it as is, otherwise treat it as a tagname
+            element = crel.isElement(element) ? element : d.createElement(element);
+            // Check if second argument is a settings object
+            if (isType(settings, 'object') && !crel[isNodeString](settings) && !Array.isArray(settings)) {
+                // Don't treat settings as a child
+                index++;
+                // Go through settings / attributes object, if it exists
+                for (key in settings) {
+                    // Store the attribute into a variable, before we potentially modify the key
+                    attribute = settings[key];
+                    // Get mapped key / function, if one exists
+                    key = crel.attrMap[key] || key;
+                    // Note: We want to prioritise mapping over properties
+                    if (isType(key, func)) {
+                        key(element, attribute);
+                    } else if (isType(attribute, func)) { // ex. onClick property
+                        element[key] = attribute;
+                    } else {
+                        // Set the element attribute
+                        element.setAttribute(key, attribute);
+                    }
                 }
             }
-        }
-        // Loop through all arguments, if any, and append them to our element if they're not `null`
-        for (; index < args.length; index++) {
-            appendChild(element, args[index]);
-        }
+            // Loop through all arguments, if any, and append them to our element if they're not `null`
+            for (; index < args.length; index++) {
+                appendChild(element, args[index]);
+            }
 
-        return element;
-    }
-
-    // Used for mapping attribute keys to supported versions in bad browsers, or to custom functionality
+            return element;
+        }, {// Binds specific tagnames to crel function calls with that tag as the first argument
+            get: (target, key) => {
+                !(key in target) && (target[key] = target.bind(null, key));
+                return target[key];
+            }
+        });
+    // Used for mapping attribute keys to custom functionality, or to supported versions in bad browsers
     crel.attrMap = {};
     crel.isElement = object => object instanceof Element;
     crel[isNodeString] = node => node instanceof Node;
-    // Expose proxy interface
-    crel.proxy = new Proxy(crel, {
-        get: (target, key) => {
-            !(key in target) && (target[key] = target.bind(null, key));
-            return target[key];
-        }
-    });
     // Export crel
     exporter(crel, func);
 })((product, func) => {
